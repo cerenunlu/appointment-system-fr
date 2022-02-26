9f857949ca3794607c3641eb19efc82b0e194d3b
cerenunlu <cerennlu01@outlook.com>
2022-02-26 15:25:06 +0300

    index.js , department.js added

---
+++ src/context/departments/index.js
@@ -0,0 +1,38 @@
+import { createContext, useContext, useState } from "react";
+import { get_departments_list_request } from "../../api";
+
+const DepartmentsContext = createContext();
+export function DepartmentsProvider({ children }) {
+    let INITIAL_STATE = { // global states
+        departments_list: [],
+    };
+
+    const [state, set_state] = useState(INITIAL_STATE);
+
+    const get_departments_list = async () => {
+        let response = await get_departments_list_request();
+        console.log('response', response)
+        set_state((prevState) => ({
+            ...prevState,
+            departments_list: response
+        }))
+    };
+
+    return (
+        <DepartmentsContext.Provider
+            value={{ // export this object
+                departments_context: state,
+                get_departments_list
+            }}
+        >
+            {children}
+        </DepartmentsContext.Provider>
+    );
+}
+export function useDepartmentsContext() {
+    const state = useContext(DepartmentsContext);
+    if (state === undefined) {
+        throw new Error("PostsContext must be used within a PostsProvider");
+    }
+    return useContext(DepartmentsContext);
+}
---
+++ src/context/index.js
@@ -0,0 +1,4 @@
+export {
+    DepartmentsProvider,
+    useDepartmentsContext
+} from "./departments";
