9d0541d4b472443ea30286b1e958ac0d83f6ab6c
cerenunlu <cerennlu01@outlook.com>
2022-02-26 15:23:23 +0300

    BrowserRouter and DepartmentsProvider added

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { DepartmentsProvider } from './context/departments';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <DepartmentsProvider>
+        <App />
+      </DepartmentsProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
