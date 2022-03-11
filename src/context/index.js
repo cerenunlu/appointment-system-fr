56cbab877597ee56363de488be24449e2900a0dc
cerenunlu <cerennlu01@outlook.com>
2022-03-11 14:02:05 +0300

    exports

--- src/context/index.js
+++ src/context/index.js
@@ -2,3 +2,12 @@ export {
     DepartmentsProvider,
     useDepartmentsContext
 } from "./departments";
+
+export {
+    EmployeesProvider,
+    useEmployeesContext
+} from "./employees";
+export {
+    CustomersProvider,
+    useCustomersContext
+} from "./customers";
