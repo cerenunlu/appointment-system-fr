805b61ec2876b3c9644c18738d6eb53265edf37b
cerenunlu <cerennlu01@outlook.com>
2022-03-11 13:54:19 +0300

    register context

---
+++ src/context/register/index.js
@@ -0,0 +1,62 @@
+import { createContext, useContext, useState } from "react";
+import { post_customer_register_request } from "../../api/Auth/register";
+import { token_storage } from "../../helpers";
+import { useNavigate } from "react-router-dom";
+const AuthContext = createContext({});
+
+
+const { set_access_token, get_access_token, delete_token_data } = token_storage;
+
+export const AuthProvider = ({ children }) => {
+    let navigate = useNavigate();
+    let INITIAL_STATE = { // global states
+        response_login: [],
+    };
+    const [state, set_state] = useState(INITIAL_STATE);
+    const post_register = async (customer_data) => {
+        if(customer_data.password==customer_data.confirm_password){
+            
+        }
+        let isloggedIn = false
+        let response = await post_customer_register_request(customer_data);
+        console.log("data.data")
+        console.log(response)
+        const loggedIn = Object.values(response);
+        if (loggedIn[0] == "success") {
+            console.log("basarili!!!!", loggedIn[0])
+            set_access_token(loggedIn[1]);
+            const x = get_access_token();
+            console.log(x);
+            delete_token_data();
+            navigate('/admin-dash');
+
+
+        } else {
+            console.log("basarisiz!!!!", loggedIn[0])
+            return isloggedIn;
+        }
+
+    };
+
+    return (
+
+        <AuthContext.Provider value={{ // export this object
+
+            post_login
+        }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+
+
+
+export function useAuthContext() {
+    const state = useContext(AuthContext);
+
+    if (state === undefined) {
+        throw new Error("PostsContext must be used within a PostsProvider");
+    }
+    return useContext(AuthContext);
+}
