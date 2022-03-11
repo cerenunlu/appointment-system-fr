97ec106f082e1c39b8268d979f063a3c9fc37424
cerenunlu <cerennlu01@outlook.com>
2022-03-11 13:46:19 +0300

    post login request

---
+++ src/api/Auth/login.js
@@ -0,0 +1,9 @@
+import { fetch } from "../fetch";
+
+export const post_login_request = async (logindata) => {
+   
+    const { data } = await fetch().post(`/auth/signin`,logindata);
+  
+  
+    return data
+}
