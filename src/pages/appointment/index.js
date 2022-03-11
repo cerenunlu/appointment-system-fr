100f25e07d63526620b084702482af9a7184b189
cerenunlu <cerennlu01@outlook.com>
2022-03-11 13:56:22 +0300

    appointment page

---
+++ src/pages/appointment/index.js
@@ -0,0 +1,116 @@
+import React, { useState } from 'react'
+import { useEffect } from "react";
+import { Grid, Paper, Avatar, Typography, Link } from '@mui/material'
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Button from '@mui/material/Button';
+import HailIcon from '@mui/icons-material/Hail';
+import LoginIcon from '@mui/icons-material/Login';
+import Box from '@mui/material/Box';
+import Input from '@mui/material/Input';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import EmojiPeopleIcon from '@mui/icons-material/EmojiPeople';
+import FormControl from '@mui/material/FormControl';
+import TextField from '@mui/material/TextField';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import { width } from '@mui/system';
+import { useAuthContext } from "../../context/Auth";
+import { token_storage } from "../../helpers/index";
+import { Navigate } from "react-router-dom";
+import Navbar from '../../components/Sidebar/navbar';
+import MultipleSelect from '../../components/MultipleSelect/index';
+
+
+import { useDepartmentsContext } from "../../context";
+import { useEmployeesContext } from "../../context/";
+
+function CreateAppointment() {
+    //STYLE//
+    const customerPaperStyle = { padding: 20, height: '70vh', width: 600, margin: '40px auto', backgroundColor: '#eeeeee' }
+    const avatarEmployeeStyle = { backgroundColor: '#64b5f6' }
+    const avatarCustomerStyle = { backgroundColor: '#2196f3' }
+    const textStyle = { margin: '20px 0' }
+    const btnStyle = { margin: '8px 0' }
+    const RegisterbtnStyle = { margin: '10px 8px', backgroundColor: '#bdbdbd' }
+    const btnEmployeeStyle = { margin: '8px 0', height: '20vh' }
+    const inputTextStyle = { margin: '10px 0', width: '350px', backgroundColor: '#9e9e9e' }
+    //DEPARTMENTS CONST
+    const { get_departments_list, departments_context } = useDepartmentsContext();
+    const { departments_list } = departments_context;
+    console.log('departments list!!', departments_list)
+    const department_select_header = "Department"
+    //EMPLOYEES CONST 
+    const { get_employees_by_departmentid, employees_context } = useEmployeesContext();
+    const { employees_list } = employees_context;
+    console.log('employees_list', employees_list)
+    const employee_select_header = "Employees"
+
+///department///
+    const get_departments_data = async () => {
+
+        await get_departments_list()
+    }
+    useEffect(() => {
+        get_departments_data()
+    }, [])
+    const [data, setData] = useState('');
+
+    const childToParent = (childdata) => {
+        setData(childdata);
+        console.log("selected dp id : ",childdata);
+    }
+
+/////////
+///employee///
+
+const get_employees_data = async () => {
+    await get_employees_by_departmentid(data)
+  }
+  useEffect(() => {
+    get_employees_data(data)
+  }, [])
+  console.log(employees_list);
+////////////
+
+    return (
+        <>
+            <Navbar />
+            <Grid container rowSpacing={0} columnSpacing={{ xs: 0, sm: 0, md: 0 }}>
+
+                <Grid item xs={12}>
+                    <Paper elevation={10} style={customerPaperStyle}>
+                        <Grid align='center'>
+                            <Avatar style={avatarEmployeeStyle}><EmojiPeopleIcon /></Avatar>
+                            <h2>Register</h2>
+                        </Grid>
+
+                        <Box sx={{ '& > :not(style)': { m: 1 } }}>
+                            <form>
+
+                                <MultipleSelect childToParent={childToParent} list={departments_list} select_header={department_select_header} />
+                                <br />
+                                <MultipleSelect list={employees_list} select_header={department_select_header} />
+                                <br />
+                                <Button variant="contained" type="submit" value="Submit" fullWidth>Create</Button>
+
+                            </form>
+
+                        </Box>
+
+
+                    </Paper>
+
+
+                </Grid>
+
+
+            </Grid>
+        </>
+
+
+    )
+
+}
+export default CreateAppointment
