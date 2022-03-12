439d02b28a22b8ba2784ffa9469d9aeef8eb9dfd
cerenunlu <cerennlu01@outlook.com>
2022-03-12 12:40:03 +0300

    refactor

--- src/api/department/departments.js
+++ src/api/department/departments.js
@@ -2,8 +2,5 @@ import { fetch } from "../fetch";
 
 export const get_departments_list_request = async () => {
     const { data } = await fetch().get(`/department`);
-    console.log(data);
-    console.log("test api")
-  
     return data
 }
