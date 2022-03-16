0ff1639897210d250f5125940090cf4f5607d94f
cerenunlu <cerennlu01@outlook.com>
2022-03-16 13:37:11 +0300

    refactor

--- src/api/Auth/register.js
+++ src/api/Auth/register.js
@@ -2,6 +2,5 @@ import { fetch } from "../fetch";
 
 export const post_customer_register_request = async (customer_data) => {
     const { data } = await fetch().post(`/customer`,customer_data);
-  
     return data
 }
