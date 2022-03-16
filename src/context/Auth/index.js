bd1f3dc2b6cf1fa208cfed1341d24443d0a3e7af
cerenunlu <cerennlu01@outlook.com>
2022-03-16 13:41:47 +0300

    refactor

--- src/context/Auth/index.js
+++ src/context/Auth/index.js
@@ -1,73 +1,72 @@
 import { createContext, useContext, useState } from "react";
 import { post_login_request } from "../../api/Auth/login";
 import { post_customer_register_request } from "../../api/Auth/register";
-import { token_storage } from "../../helpers";
+import { token_storage,user_storage } from "../../helpers";
 import { useNavigate } from "react-router-dom";
 const AuthContext = createContext({});
 
-
 const { set_access_token, get_access_token, delete_token_data } = token_storage;
-
+const {set_user_tokens,get_user_data}=user_storage;
 export const AuthProvider = ({ children }) => {
-    let navigate = useNavigate();
-    let INITIAL_STATE = { // global states
-        response_login: [],
-    };
-    const [state, set_state] = useState(INITIAL_STATE);
-    const post_login = async (login_data) => {
-        let response = await post_login_request(login_data);
-        
-        let isloggedIn = false
-        const loggedIn = Object.values(response);
-        if (loggedIn[0] == "success") {
-            set_access_token(loggedIn[1]);
-            const x = get_access_token();
-            navigate('/admin-dash');
+  let navigate = useNavigate();
+  let INITIAL_STATE = {
+    // global states
+    response_login: [],
+  };
+  const [state, set_state] = useState(INITIAL_STATE);
+  const post_login = async (login_data) => {
+    let response = await post_login_request(login_data);
 
+    let isloggedIn = false;
+    const loggedIn = Object.values(response);
+    if (loggedIn[0] == "success") {
+      set_access_token(loggedIn[1]);
+      set_user_tokens(loggedIn[1]);
+      const x = get_access_token();
+      navigate("/admin-dash");
+      return response;
+    } else {
+      return loggedIn;
+    }
+  };
+  const post_register = async (customer_data) => {
+    if (customer_data.password == customer_data.confirm_password) {
+      let response = await post_customer_register_request(customer_data);
+      const loggedIn = Object.values(response);
+      if (loggedIn[0] == "success") {
+        console.log("basarili!!!!", loggedIn[0]);
+        set_access_token(loggedIn[1]);
+        const x = get_access_token();
+        const user=get_user_data();
+        console.log(x);
+        console.log("userdata",user);
+        navigate("/admin-dash");
+      } else {
+        console.log("basarisiz!!!!", loggedIn[0]);
+        return loggedIn;
+      }
+    }
+  };
 
-        } else {
-            return loggedIn;
-        }
+  return (
+    <AuthContext.Provider
+      value={{
+        // export this object
 
-    };
-    const post_register = async (customer_data) => {
-        if (customer_data.password == customer_data.confirm_password) {
-            let response = await post_customer_register_request(customer_data);
-            const loggedIn = Object.values(response);
-            if (loggedIn[0] == "success") {
-                console.log("basarili!!!!", loggedIn[0])
-                set_access_token(loggedIn[1]);
-                const x = get_access_token();
-                console.log(x);
-                navigate('/admin-dash');
-
-            } else {
-                console.log("basarisiz!!!!", loggedIn[0])
-                return loggedIn;
-            }
-        }
-
-    };
-
-    return (
-
-        <AuthContext.Provider value={{ // export this object
-
-            post_login, post_register
-        }}>
-            {children}
-        </AuthContext.Provider>
-    )
-}
-
-
-
+        post_login,
+        post_register,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
 
 export function useAuthContext() {
-    const state = useContext(AuthContext);
+  const state = useContext(AuthContext);
 
-    if (state === undefined) {
-        throw new Error("PostsContext must be used within a PostsProvider");
-    }
-    return useContext(AuthContext);
+  if (state === undefined) {
+    throw new Error("PostsContext must be used within a PostsProvider");
+  }
+  return useContext(AuthContext);
 }
