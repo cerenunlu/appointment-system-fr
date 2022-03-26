1a9a855c21b57ecfd6cb56a75533d90aebb3ed8e
cerenunlu <cerennlu01@outlook.com>
2022-03-26 14:53:27 +0300

    error page added

---
+++ src/pages/error/index.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+function Error() {
+  return (
+    <>
+      <div>
+        <h1> Page not found!</h1>
+      </div>
+    </>
+  );
+}
+
+export default Error;
